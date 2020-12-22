e04d4d032e3f2aa5be09258d774e09cadf2ca0dd
MounirBlk <mounir.ballouk@gmail.com>
2020-12-22 15:37:51 +0100

    avancement rpg infos

--- model_rpg/item.py
+++ model_rpg/item.py
@@ -1,6 +1,6 @@
-
 from math import floor
 from random import randint
+from utils import LIST_ITEMS
 
 """ Class Item """
 
@@ -45,130 +45,9 @@ class Item():
     def __str__(self):
         return "Key:"+str(self.objet["key"])+"\n"+"Name:"+self.name+"\n"+"Type items:"+str(self.objet["catg"])+"\n"+"Attaque:"+str(self.attaque)+"\n"+"Defense:"+str(self.defense)+"\n"+"Vitesse:"+str(self.vitesse)+"\n"+"Chance:"+str(self.chance)+"\n"+"Mana:"+str(self.mana)+"\n"+"Vie:"+str(self.vie)+"\n"
 
-LIST_ITEMS = {
-    "Epee": {
-        "key": 1,
-        "catg": 1,
-        "type_personnage": 1,
-    },
-    "Arc": {
-        "key": 2,
-        "catg": 1,
-        "type_personnage": 1,
-    },
-    "Baton": {
-        "key": 3,
-        "catg": 1,
-        "type_personnage": 3,
-    },
-    "Dague": {
-        "key": 4,
-        "catg": 1,
-        "type_personnage": 2,
-    },
-    "Casque": {
-        "key": 5,
-        "catg": 2,
-        "type_personnage": 1,
-    },
-    "Bouclier": {
-        "key": 6,
-        "catg": 2,
-        "type_personnage": 1,
-    },
-    "Fumigene": {
-        "key": 7,
-        "catg": 2,
-        "type_personnage": 2,
-    },
-    "AirJordan": {
-        "key": 8,
-        "catg": 3,
-        "type_personnage": 2,
-    },
-    "Moto": {
-        "key": 9,
-        "catg": 3,
-        "type_personnage": 3,
-    },
-    "Batmobile": {
-        "key": 10,
-        "catg": 3,
-        "type_personnage": 2,
-    },
-    "Cheval": {
-        "key": 11,
-        "catg": 3,
-        "type_personnage": 1,
-    },
-    "Trefle": {
-        "key": 12,
-        "catg": 4,
-        "type_personnage": 2,
-    },
-    "Fer": {
-        "key": 13,
-        "catg": 4,
-        "type_personnage": 1,
-    },
-    "As": {
-        "key": 14,
-        "catg": 4,
-        "type_personnage": 3,
-    },
-    "Lapin": {
-        "key": 15,
-        "catg": 4,
-        "type_personnage": 2,
-    },
-    "Grigri": {
-        "key": 16,
-        "catg": 5,
-        "type_personnage": 2,
-    },
-    "Talisman": {
-        "key": 17,
-        "catg": 5,
-        "type_personnage": 1,
-    },
-    "Baguette": {
-        "key": 18,
-        "catg": 5,
-        "type_personnage": 3,
-    },
-    "Grimoire": {
-        "key": 19,
-        "catg": 5,
-        "type_personnage": 3,
-    },
-    "Breuvage": {
-        "key": 20,
-        "catg": 6,
-        "type_personnage": 1,
-    },
-    "Potion": {
-        "key": 21,
-        "catg": 6,
-        "type_personnage": 3,
-    },
-    "Defribrillateur": {
-        "key": 22,
-        "catg": 6,
-        "type_personnage": 2,
-    },
-    "Senzu": {
-        "key": 23,
-        "catg": 6,
-        "type_personnage": 3,
-    },
-    "Bague": {
-        "key": 24,
-        "catg": 2,
-        "type_personnage": 3,
-    },
-}
-
-
+# Test module
 if __name__ == "__main__":
-    item = Item(10, 1)
+    level = 50
+    item = Item(level, 1)
+    print('Mon level est: '+str(level))
     print(item)
